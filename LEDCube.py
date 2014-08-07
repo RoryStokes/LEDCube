e97d9821cbe907551d5d0324a9502b54413ede4e
Rory Stokes <contact@rorystok.es>
2014-08-07 11:47:37 +0930

    Bugfixes, pin addressing

--- LEDCube.py
+++ LEDCube.py
@@ -4,20 +4,17 @@ import threading
 import numpy as np
 import Queue
 
-
-
 class LEDCube:
 	class CubeThread(threading.Thread):
 		def __init__(self,n,dt):
+			self.pins = [3,5,7,11,13,15,19,21,23,29,31,33,35,37,8,10,12,16,18,22,24,26,32,36,38,40]
 			threading.Thread.__init__(self)
-			GPIO.setmode(GPIO.BCM)
-			for i in range(1,27):
-				GPIO.setup(i, GPIO.OUT)
-			
-			GPIO.output(26,0)
+			GPIO.setmode(GPIO.BOARD)
+			for pin in self.pins:
+				GPIO.setup(pin, GPIO.OUT)
 
 			self.lattice = np.zeros(n,n,n)
-			self.h = n
+			self.n = n
 
 			self.dt = dt
 			self.q = Queue.Queue()
@@ -25,20 +22,20 @@ class LEDCube:
 
 		def run(self):
 			while(True):
-				while !q.empty():
-					e = q.get()
+				while not self.q.empty():
+					e = self.q.get()
 					if e = 'Q':
 						break
 
-				for x in range(0,n):
-					for y in range(0,n):
-						bcm = 1 + y*n + x
-						GPIO.output(bcm, lattice[self.layer,x,y])
+				for x in range(0,self.n):
+					for y in range(0,self.n):
+						i = 1 + y*self.n + x
+						GPIO.output(self.pins[i], self.lattice[self.layer,x,y])
 
 				GPIO.output(26,1)
-				self.layer = (self.layer+1)%self.h
+				self.layer = (self.layer+1)%self.n
 				GPIO.output(26,0)
-				time.sleep(self.dt)
+				time.sleep(self.dt/1000)
 
 	def __init__(self,n,dt):
 		self.thread = LEDCube.CubeThread(n,dt)
